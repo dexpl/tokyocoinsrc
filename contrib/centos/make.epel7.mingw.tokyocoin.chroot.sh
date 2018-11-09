6acd311ec8ab7fd18571949c2ca86d9bc57dda26
Vadim Raskhozhev <iamdexpl@gmail.com>
2018-11-09 03:51:44 +0300

    `make -f makefile.linux-mingw.patch` successfully builds a tokyocoind.exe which at least does not crash immeditely at startup

--- contrib/centos/make.epel7.mingw.tokyocoin.chroot.sh
+++ contrib/centos/make.epel7.mingw.tokyocoin.chroot.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
-tc=$(rpm -E %_srcrpmdir)/tokyocoin-1.1-4.fc29.src.rpm
-basedir=${HOME}/projects/tc.mingw/static
-makefile=${HOME}/projects/tokyocoinsrc/src/makefile.linux-mingw
-deps="$(rpm -E %_rpmdir)/noarch/mingw32-boost-1.60.0-1.el7.noarch.rpm"
-deps+=" $(rpm -E %_rpmdir)/noarch/mingw32-boost-static-1.60.0-1.el7.noarch.rpm"
-deps+=" $(rpm -E %_rpmdir)/noarch/mingw32-libdb-5.3.28-3.el7.noarch.rpm"
-deps+=" $(rpm -E %_rpmdir)/noarch/mingw32-libdb-static-5.3.28-3.el7.noarch.rpm"
+# Legacy:
+#tc=$(rpm -E %_srcrpmdir)/tokyocoin-1.1-4.fc29.src.rpm
+#basedir=${HOME}/projects/tc.mingw
+#mock="mock --config-opts=basedir=${basedir} -r ${chrootconf}"
+#[ -d "${basedir}" ] || mkdir -p "${basedir}"
+#${mock} --chroot "chmod +x ${srcbasedir}/leveldb/build_detect_platform"
+#${mock} --chroot "rpm -ihv /$(basename ${tc})"
+#${mock} --chroot 'rpmbuild -bp --nodeps $(rpm -E %_specdir/tokyocoin.spec)'
+#srcbasedir=$(${mock} --chroot 'rpm -E %_builddir')/tokyocoinsrc-1.1/src/
+#[ -f "${makefile}" ] && ${mock} --copyin "${makefile}" ${srcbasedir}
+
+set -e
+
+mydir="$(dirname "$(readlink -f "${0}")")"
+topdir="$(git -C "${mydir}" rev-parse --show-toplevel)"
+chrootconf="${mydir}/epel-7-x86_64-trashcoin.cfg"
+makefile=makefile.linux-mingw
+deps+=" mingw32-boost-static"
+deps+=" mingw32-libdb-static"
 deps+=" mingw32-openssl-static"
 deps+=" mingw32-winpthreads-static"
 deps+=" mingw32-zlib-static"
-mock="mock --config-opts=basedir=${basedir} -r epel-7-x86_64"
+mock="mock -r ${chrootconf}"
+mockroot=$(${mock} --print-root-path)
 
-set -e
-[ -d "${basedir}" ] || mkdir -p "${basedir}"
-[ -f $(${mock} --print-root-path)/.initialized ] || ${mock} --init
+cd "${topdir}"
+gitbranch=$(git rev-parse --abbrev-ref HEAD)
+archivedir="/tmp/tokyocoin.${gitbranch}"
+chrootarchivedir="$(basename "${archivedir}")"
+chrootsrcdir="${chrootarchivedir}/src"
+[ -d "${archivedir}" ] && find "${archivedir}" -delete
+mkdir "${archivedir}"
+git archive --format=tar ${gitbranch} | tar -xC "${archivedir}"
+[ -f ${mockroot}/.initialized ] || ${mock} --init
+[ -e ${mockroot}/${chrootarchivedir} ] && ${mock} --chroot "find ${chrootarchivedir} -delete"
+${mock} --copyin "${archivedir}" /
 ${mock} --install ${deps}
-${mock} --copyin ${tc} /
-${mock} --chroot "rpm -ihv /$(basename ${tc})"
-${mock} --chroot 'rpmbuild -bp --nodeps $(rpm -E %_specdir/tokyocoin.spec)'
-srcbasedir=$(${mock} --chroot 'rpm -E %_builddir')/tokyocoinsrc-1.1/src/
-${mock} --chroot "chmod +x ${srcbasedir}/leveldb/build_detect_platform"
-[ -f "${makefile}" ] && ${mock} --copyin "${makefile}" ${srcbasedir}
 echo ${mock}
+${mock} --chroot "cd ${chrootsrcdir}; make -f ${makefile} USE_UPNP=- STATIC=1"
